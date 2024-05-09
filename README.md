4fb6fda1667ccbeb57e06fb32373bdb4b3b56a77
Neilikhan <162450207+Neilikhan@users.noreply.github.com>
2024-05-10 02:45:36 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+#neili-Calculator
+neili Calculator This project is a simple calculator implemented in TypeScript. It provides essential arithmetic operations and a few additional features. You can use it for quick calculations without any complex functionalities.
+
+Features Arithmetic Operations: Addition (+) Subtraction (-) Multiplication (*) Division (/) Usage Clone this repository to your local machine. Open the project in your preferred TypeScript editor. Run the TypeScript compiler to generate the JavaScript files. Open index.html in your browser to use the calculator.
+
+License This project is licensed under the MIT License. Feel free to use, modify, and distribute it as needed.
---
+++ index.ts
@@ -0,0 +1,51 @@
+#!/user/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers:{
+    numberOne: number,
+    numberTwo: number,
+    operator: string,
+} =await inquirer.prompt([
+        {type:"number",
+            name:"numberOne",
+            message:"Enter your first number"},
+            {type:"number",
+                name:"numberTwo",
+                message:"Enter your second number"},
+                {type:"list",
+                    name:"operator",
+                    message:"select an operator",
+                    choices:["+","-","*","/","^","sqrt","%"]},
+    ]);
+
+    const{numberOne, numberTwo, operator}= answers
+    let result;
+
+    switch (operator){
+        case "+":
+            result= numberOne + numberTwo;
+            break;
+        case "-":
+            result= numberOne - numberTwo;
+            break;
+        case "*":
+            result= numberOne * numberTwo;
+            break;
+        case "/":
+            result= numberOne / numberTwo;
+            break;
+        case "^":
+            result= Math.pow(numberOne, numberTwo);
+            break;
+        case "sqrt":
+            result= Math.sqrt (numberOne);
+            break;
+        case "%":
+                result= numberOne % numberTwo;
+        default:
+                console.log("invalid operator");
+                break;
+
+    }
+    console.log(chalk.yellow(`${numberOne}${operator}${numberTwo}=${result}`));
